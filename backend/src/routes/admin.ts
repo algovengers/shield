52b3b2b18d196274f227343853b84f45571c40a5
subharthi75 <subharthi75@gmail.com>
2024-08-24 22:56:11 +0530

    auuuth

--- backend/src/routes/admin.ts
+++ backend/src/routes/admin.ts
@@ -1,12 +1,14 @@
 import { loginAdmin, logoutAdmin, registerAdmin } from "controllers/admin.controller";
+import { getSos } from "controllers/sos.controller";
 import { Router } from "express";
 import { authenticateAdmin } from "middleware/auth";
 
 const adminRouter = Router()
 
 adminRouter.route('/login').post(loginAdmin)
-adminRouter.route('/create').post(authenticateAdmin,registerAdmin)
-adminRouter.route('/logout').post(authenticateAdmin,logoutAdmin)
+adminRouter.route('/create').post(registerAdmin)
+adminRouter.route('/logout').post(logoutAdmin)
+adminRouter.route('/getsos').post(getSos)
 // adminRouter.route('/dashboard').post()
 
 export {adminRouter}
